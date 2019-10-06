14122c2c0a2a9232cce671f95197d3e8cf8030a1
visual233 <264050673@qq.com>
2019-10-06 00:35:10 -0400

    Add files via upload

--- encryptedIMclient.py
+++ encryptedIMclient.py
@@ -1,11 +1,11 @@
 import argparse
 import socket
 import select
-import prototest_pb2
+import basicim_nugget_pb2
 import sys
 import struct
 from Crypto.Cipher import AES
-from binascii import b2a_hex, a2b_hex
+import random, string
 import hashlib
 import hmac
 
@@ -16,28 +16,35 @@ parser.add_argument('-c', dest='confidentialitykey', required=True)
 parser.add_argument('-a', dest='authenticitykey', required=True)
 
 args = parser.parse_args()
+iv = ''.join(random.choice(string.ascii_letters+string.digits) for _ in range(16)).encode('utf-8')
 
-def encryption(s):
-    key = args.confidentialitykey
-    cipher = AES.new(key, AES.MODE_CBC, key)
+def add_length(s):
     l = 32
     count = len(s)
     add = 0
     if count % l != 0:
-        add = l - (count%l)
-    s += '\0'*add
+        add = l - (count % l)
+    s += '\0' * add
+    return s
+
+def encryption(s, key):
+    # key = args.confidentialitykey
+    s = add_length(s)
+    key = add_length(key)
+    cipher = AES.new(key.encode('utf-8'), AES.MODE_CBC, iv)
     cipher_text = cipher.encrypt(s)
-    return b2a_hex(cipher_text)
+    return cipher_text
 
-def decryption(c_s):
-    key = args.confidentialitykey
-    cipher = AES.new(key, AES.MODE_CBC, key)
-    plain_text = cipher.decrypt(a2b_hex(c_s))
-    return plain_text
+def decryption(c_s, key):
+    # key = args.confidentialitykey
+    key = add_length(key)
+    cipher = AES.new(key.encode('utf-8'), AES.MODE_CBC, iv)
+    plain_text = cipher.decrypt(c_s)
+    return bytes.decode(plain_text).rstrip('\0')
 
-def authentic_encode(s):
-    k = args.authenticitykey
-    hm = hmac.new(k, s, hashlib.sha256)
+def authentic_encode(s, key):
+    # k = args.authenticitykey
+    hm = hmac.new(key.encode('utf-8'), s, hashlib.sha256)
     return hm.hexdigest()
 
 
@@ -48,6 +55,8 @@ def main():
     s.connect( (args.server,9999) )
 
     read_fds = [ sys.stdin, s ]
+    key_e = args.confidentialitykey
+    key_a = args.authenticitykey
 
     while True:
         (ready_list,_,_) = select.select(read_fds,[],[])
@@ -56,12 +65,14 @@ def main():
             if user_input.rstrip().lower() == "exit":
                 s.close()
                 exit(0)
-            nugget = prototest_pb2.BasicIMNugget()
-            nugget.nickname = args.nickname
+            nugget = basicim_nugget_pb2.BasicIMNugget()
             #encrypt message
-            en_message = encryption(user_input)
-            au_message = authentic_encode(en_message)
-            nugget.message = au_message + ',' + en_message
+            en_message = encryption(user_input, key_e)
+            en_name = encryption(args.nickname, key_e)
+            nugget.nickname = en_name
+            nugget.au_message = authentic_encode(en_message+iv, key_a)
+            nugget.en_message = en_message
+            nugget.iv = iv
 
             serialized = nugget.SerializeToString()
             serialized_len = len(serialized)
@@ -71,14 +82,16 @@ def main():
             packed_len = s.recv(2,socket.MSG_WAITALL)
             unpacked_len = struct.unpack("!H", packed_len )[0]
             serialized = s.recv(unpacked_len,socket.MSG_WAITALL)
-            nugget = prototest_pb2.BasicIMNugget()
+            nugget = basicim_nugget_pb2.BasicIMNugget()
             nugget.ParseFromString(serialized)
 
             #verify and decrypt
-            receive = nugget.message.split(',')
-            auth = authentic_encode(receive[1])
-            if auth == receive[0]:
-                m = decryption(receive[1])
+            enm = nugget.en_message
+            aum = nugget.au_message
+            r_iv = nugget.iv
+            auth = authentic_encode(enm+r_iv, key_a)
+            if auth == aum:
+                m = decryption(enm, key_e)
                 print("%s: %s" % (nugget.nickname, m), flush=True)
             else:
                 s.close()
@@ -87,4 +100,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
