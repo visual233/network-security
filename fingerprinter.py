c650afb228735347044836e10a9114b55a12e170
visual233 <264050673@qq.com>
2019-11-20 16:35:29 -0500

    Add files via upload

--- fingerprinter.py
+++ fingerprinter.py
@@ -9,13 +9,18 @@ import scapy.all as sc
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
+import argparse
 
-pks = sc.rdpcap('pcaps/ssl/tor-ssl5.pcap')
+parser = argparse.ArgumentParser()
+parser.add_argument('file', help='pacp file')
+args = parser.parse_args()
+
+pks = sc.rdpcap(args.file)
 x = 0
 for p in pks:
     x += len(p['TCP'].payload)
     
-print(x)
+dic = {'bing':0, 'canvas':0, 'craigslist':0, 'autolab':0, 'neverssl':0, 'tor':0, 'wikipedia':0}
 
 df = pd.read_csv('info.csv')
 X_train = np.array(df['length']).reshape(35,1)
@@ -25,5 +30,9 @@ Y_train = np.array(df['dist'])
 model = KNeighborsClassifier(n_neighbors=4)
 model.fit(X_train, Y_train)
 
-y = model.predict([[x]])
-print(y)
+y = model.predict_proba([[x]])
+
+for k,p in zip(dic.keys(),y[0]):
+    dic[k] = p
+print(dic)
+
